74aec739cd02e89a0541279f130f5f9f5ba2b433
Wildfalcon <laurie@wildfalcon.com>
2010-08-06 23:56:21 +0100

    Removed "hello world" line

--- lib/generators/abingo_migration/abingo_migration_generator.rb
+++ lib/generators/abingo_migration/abingo_migration_generator.rb
@@ -2,7 +2,6 @@ class AbingoMigrationGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
   
   def generate_migration
-    puts "Hello world!: #{migration_timestamp}"
     copy_file "create_abingo_tables.rb", "db/migrate/#{migration_timestamp}_create_abingo_tables.rb" 
   end
   
