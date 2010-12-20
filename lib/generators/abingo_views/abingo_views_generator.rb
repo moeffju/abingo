68a91ff6f8b56a7009e44585b58cf9a132256297
Matthias Bauer <moeffju@gmail.com>
2010-12-20 17:13:05 +0100

    Fix path references

--- lib/generators/abingo_views/abingo_views_generator.rb
+++ lib/generators/abingo_views/abingo_views_generator.rb
@@ -1,9 +1,8 @@
 class AbingoViewsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-
   def copy_views
-    copy_file "views/dashboard/_experiment.html.erb", "app/views/dashboard/_experiment.html.erb" 
-    copy_file "views/dashboard/index.html.erb", "app/views/dashboard/index.html.erb" 
+    copy_file "views/ab_dashboard/_experiment.html.erb", "app/views/ab_dashboard/_experiment.html.erb" 
+    copy_file "views/ab_dashboard/index.html.erb", "app/views/ab_dashboard/index.html.erb" 
   end
 end
